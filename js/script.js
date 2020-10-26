57149b3190071778250348cae7c128ba3cc64afd
Peter Gacek <peterwgacek@gmail.com>
2020-10-26 13:57:25 -0400

    adds .ajax

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
+console.log(`It's working!`)
+
 /*----- constants -----*/
+$.ajax({
+    url:'https://api.themoviedb.org/3/movie/425001-the-war-with-grandpa?api_key=9813870ae074d390419314b3cbc61171&language=en-US'
+  }).then(
+    (data) => {
+     console.log(data.genres);
+    },
+    (error) => {
+     console.log('bad request', error);
+    }
+  );
 
 /*----- app's state (variables) -----*/
 
