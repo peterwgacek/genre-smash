1aa90124805dbc5caa8134ba0754a6c63c405040
Peter Gacek <peterwgacek@gmail.com>
2020-10-27 13:51:02 -0400

    attempting to search API

--- js/script.js
+++ js/script.js
@@ -3,14 +3,17 @@ console.log("It's working!")
 
 function handleGetData(event){
   event.preventDefault();
+
+  //Load in the value of the search textbox
   const searchText = $("#search").val()
                 $.ajax({
-                    url: 'https://api.themoviedb.org/3/movie/425001-the-war-with-grandpa?api_key=9813870ae074d390419314b3cbc61171&language=en-US'
+                    url: `https://api.themoviedb.org/3/search/movie?api_key=9813870ae074d390419314b3cbc61171&query=${searchText}`
+                    
                 })
                 .then(
                     (data) => {
                         console.log(data.genres);
-                        $("#genre").text(data.genres);
+                        $("#genre").text(data.genres[1].name);
                         $("#title").text(data.title);
                         $("#tagline").text(data.tagline);
                         $("#year").text(data.release_date);
