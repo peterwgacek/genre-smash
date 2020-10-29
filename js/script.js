758a77f552e05611592a56b40947a890bcdf304b
Peter Gacek <peterwgacek@gmail.com>
2020-10-29 12:35:31 -0400

    adds beautify to js

--- js/script.js
+++ js/script.js
@@ -22,43 +22,44 @@ $.ajax({
             }
         }
     )
-    function handleGetData(event) {
-        event.preventDefault()
-      
-        //Load in the value of the search textbox
-        const searchText = $('#genre').val()
-        console.log(searchText)
-        const q = encodeURI(`?with_genres=${searchText}&api_key=${key}&language=en-US`)
-        $.ajax({
-          url: `${baseUrl}/discover/movie${q}`
-        }).then(
-          data => {
+
+function handleGetData(event) {
+    event.preventDefault()
+
+    //Load in the value of the search textbox
+    const searchText = $('#genre').val()
+    console.log(searchText)
+    const q = encodeURI(`?with_genres=${searchText}&api_key=${key}&language=en-US`)
+    $.ajax({
+        url: `${baseUrl}/discover/movie${q}`
+    }).then(
+        data => {
             if (Boolean(data) && data.total_results > 0) {
-              const index = Math.floor(Math.random() * Math.floor(data.results.length - 1))
-              const movie = data.results[index]
-              $('#title').text(movie.title)
-              $('#year').text(movie.release_date)
-              $('#overview').text(movie.overview)
-              $('#user-rating').text(movie.vote_average)
-      
-              // Use jQuery to grab the image node from the dom
-              const imgNode = $('#poster_image')
-              const imgpos =  `${imgUrl}${movie.poster_path}`
-              // if we get a result (ie: it's already been added to the dom previously) just update the src
-              if (imgNode.length) {
-                imgNode.attr('src', imgpos)
-              } else {
-                // Else create the image and add it to the dom (next time it's clicked it will be there so it will just update the src attribute)
-                // notice the ID here - this is what we're using the grab the element and do the check above
-                const image = `<img id="poster_image" src=${imgpos} alt="poster"/>`
-                $('#poster').append(image)
-              }
+                const index = Math.floor(Math.random() * Math.floor(data.results.length - 1))
+                const movie = data.results[index]
+                $('#title').text(movie.title)
+                $('#year').text(movie.release_date)
+                $('#overview').text(movie.overview)
+                $('#user-rating').text(movie.vote_average)
+
+                // Use jQuery to grab the image node from the dom
+                const imgNode = $('#poster_image')
+                const imgpos = `${imgUrl}${movie.poster_path}`
+                // if we get a result (ie: it's already been added to the dom previously) just update the src
+                if (imgNode.length) {
+                    imgNode.attr('src', imgpos)
+                } else {
+                    // Else create the image and add it to the dom (next time it's clicked it will be there so it will just update the src attribute)
+                    // notice the ID here - this is what we're using the grab the element and do the check above
+                    const image = `<img id="poster_image" src=${imgpos} alt="poster"/>`
+                    $('#poster').append(image)
+                }
             } else {
-              alert('No results')
+                alert('No results')
             }
-          },
-          error => {
+        },
+        error => {
             console.log('bad request: ', error)
-          }
-        )
-      }
+        }
+    )
+}
