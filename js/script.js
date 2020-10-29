d1a142f16d30084da57c89df787dc30e0e074e9b
Peter Gacek <peterwgacek@gmail.com>
2020-10-28 21:14:09 -0400

    adds full fucntionality

--- js/script.js
+++ js/script.js
@@ -1,52 +1,60 @@
 console.log("It's working!")
 const key = '9813870ae074d390419314b3cbc61171'
 const baseUrl = 'https://api.themoviedb.org/3'
+const imgUrl = 'https://image.tmdb.org/t/p/w500'
 
 $.ajax({
-    url: `${baseUrl}/genre/movie/list?api_key=${key}&language=en-US&`
-})
-.then (
-    (data) => {
-        // result need to insert options into a html select field
-        // when suer selects an option and clikcs smash
-        // take id and pass it into the ajax call
-        const options = data.genres
-    }
-)
-/
-function handleGetData(event){
-  event.preventDefault();
- 
-  console.log(genres)
+        url: `${baseUrl}/genre/movie/list?api_key=${key}&language=en-US&`
+    })
+    .then(
+        (data) => {
+          
+            if (Boolean(data) && (data.genres || []).length) {
+                for (let i = 0; i < data.genres.length; i++) {
+                    const genre = data.genres[i]
+                    $('#genre').append(new Option(genre.name, genre.id))
 
-  //Load in the value of the search textbox
-  const searchText = $("#search").val()
-  const q = encodeURI(`?query=${searchText}&api_key=${key}&language=en-US`)
-                $.ajax({
-                    url: `${baseUrl}/search/movie${q}`
-                    
-                })
-                .then(
-                    (data) => {
-                        if(Boolean(data) && data.total_results > 0){
-                        const index = Math.floor(Math.random() * Math.floor(data.results.length - 1));
-                        const movie = data.results[index]
-                        console.log('movie', movie);
-                        } else{
-                            alert('No results')
-                        }
-                        // $("#genre").text(data.genres[1].name);
-                        // $("#title").text(data.title);
-                        // $("#tagline").text(data.tagline);
-                        // $("#year").text(data.release_date);
-                        // $("#overview").text(data.overview);
-                        // $("#user-rating").text(data.vote_average);
-                        // // $("#poster").attr("src",  data.poster_path)
-                            
-                    },
-                    (error) => {
-                        console.log("bad request: ", error)
-                      }
-                    )
                 }
-                $('form').on("submit", handleGetData)
+            }
+
+        }
+
+    )
+
+function handleGetData(event) {
+    event.preventDefault();
+
+
+    //Load in the value of the search textbox
+    const searchText = $("#genre").val()
+    console.log(searchText)
+    const q = encodeURI(`?with_genres=${searchText}&api_key=${key}&language=en-US`)
+    $.ajax({
+            url: `${baseUrl}/discover/movie${q}`
+
+        })
+        .then(
+            (data) => {
+                if (Boolean(data) && data.total_results > 0) {
+                    const index = Math.floor(Math.random() * Math.floor(data.results.length - 1));
+                    const movie = data.results[index]
+                    const imgpos = `${imgUrl}${movie.poster_path}`
+                    const image = `<img src=${imgpos} alt="poster"/>` 
+                    $("#title").text(movie.title);
+                    $("#year").text(movie.release_date);
+                    $("#overview").text(movie.overview);
+                    $("#user-rating").text(movie.vote_average);
+                    $("#poster").append(image);
+                } else {
+                    alert('No results')
+                }
+
+
+
+            },
+            (error) => {
+                console.log("bad request: ", error)
+            }
+        )
+}
+$('form').on("submit", handleGetData)
